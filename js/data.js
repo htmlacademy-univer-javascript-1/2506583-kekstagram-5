0f29bfa26f805fb2fa0df4b41f8685d232556318
Данил Тотоев <totoevdanil57@gmail.com>
2024-10-14 21:41:42 +0500

    продолжаю убирать ошибки еслинт

--- js/data.js
+++ js/data.js
@@ -22,18 +22,18 @@ const MESSAGE = [
 // Функция для генерации случаного 1 или 2 сообщений
 function getRandomMessage() {
   // Генерация случайного числа от 1 до 2
-  const countMessage = getRandomInteger(1, 2);
+    const countMessage = getRandomInteger(1, 2);
 
   let message;
   if (countMessage === 2) {
-      const firstIndex = getRandomInteger(0, MESSAGE.length - 1);
-      const secondIndex = getRandomInteger(0, MESSAGE.length - 1);
+    const firstIndex = getRandomInteger(0, MESSAGE.length - 1);
+    const secondIndex = getRandomInteger(0, MESSAGE.length - 1);
 
-      if (firstIndex !== secondIndex) {
-          message = `${MESSAGE[firstIndex]} ${MESSAGE[secondIndex]}`;
-      } else {
-          message = MESSAGE[firstIndex];
-      }
+    if (firstIndex !== secondIndex) {
+      message = `${MESSAGE[firstIndex]} ${MESSAGE[secondIndex]}`;
+    } else {
+        message = MESSAGE[firstIndex];
+    }
   } else {
       const index = getRandomInteger(0, MESSAGE.length - 1);
       message = MESSAGE[index];
@@ -47,7 +47,7 @@ const idSet = new Set();
 //const photoIdSet = new Set();
 
 function getComment(){
-  const idParam = getRandomInteger(1, 10000);
+  let idParam = getRandomInteger(1, 10000);
   const photoIdParam = getRandomInteger(1, 6);
   while(idSet.has(idParam)){
     idParam = getRandomInteger(1, 10000);
