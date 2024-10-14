8664f1e7b99841df342f2b916369e4b73c726996
Данил Тотоев <totoevdanil57@gmail.com>
2024-10-14 22:04:54 +0500

    продолжаю убирать ошибки еслинт-3

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ import { generatePhoto } from './generate-photo.js';
 
 // Создание массива объектов фотографий
 const posts = [];
-for(let i = 1; i <= 25; i++){
+for (let i = 1; i <= 25; i++) {
   const currentPhoto = generatePhoto(i);
   currentPhoto.generateComments();
   posts.push(currentPhoto);
-  }
+}
 
 
