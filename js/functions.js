fd3407cc68c6451d44d9b771f8cd6eeb09aeb808
Данил Тотоев <totoevdanil57@gmail.com>
2024-10-22 15:36:06 +0500

    функции-5.2

--- js/functions.js
+++ js/functions.js
@@ -1,42 +1,8 @@
-const checkLength = (string, maxLen) => (string.length <= maxLen);
-
-checkLength("hhhhh", 3);
-checkLength("hhhhh", 10);
-checkLength("hhhhh", 5);
-
-function isWordPalindrom(string){
-  let stringWithoutSpaces = (string.replaceAll(" ", "")).toUpperCase();
-  let newString = '';
-  for (let i = stringWithoutSpaces.length - 1; i >= 0; i--){
-    newString += stringWithoutSpaces[i];
-  }
-  let result = newString === stringWithoutSpaces;
-  return result;
+function isMeetingInWorkHours(startTimeOfDay, endTimeOfDay, meetingStartTime, meetingDurationMinutes) {
+  const start = new Date(`${startTimeOfDay}T00:00:00`);
+  const end = new Date(`${endTimeOfDay}T00:00:00`);
+  const meetingEndTime = new Date(new Date(meetingStartTime).getTime() + meetingDurationMinutes * 60 * 1000);
+  return !(meetingEndTime > end || meetingStartTime < start);
 }
 
-isWordPalindrom("тОпот");
-isWordPalindrom("Д о в о д   ");
-isWordPalindrom("Лёша на полке клопа нашёл ");
-
-function convertToNumber(string){
-  let stringWithoutSpaces = (string.replaceAll(" ", "")).split('');
-  let newString = '';
-  for (let i = 0;stringWithoutSpaces.length > i; i++){
-    if (isInteger(stringWithoutSpaces[i])){
-      newString += stringWithoutSpaces[i];
-    }
-    if (!(isInteger(stringWithoutSpaces[i]))){
-      newString += '';
-    }
-  }
-  return newString;
-}
-
-
-function isInteger(value) {
-  return /^\d+$/.test(value);
-}
-
-convertToNumber('2023 год');
-convertToNumber('1 кефир, 0.5 батона');
-convertToNumber('ECMAScript 2022');
+isMeetingInWorkHours('08:00', '17:30', '14:00', 90);
