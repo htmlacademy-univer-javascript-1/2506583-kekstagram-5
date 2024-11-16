b4870a7552ecee49ad89827fc5af843f149194ae
Данил Тотоев <totoevdanil57@gmail.com>
2024-11-16 21:58:45 +0500

    Исправляю ошибки еслинта-2

--- js/draw.js
+++ js/draw.js
@@ -1,17 +1,17 @@
 function renderPhotos(posts) {
-  let spisok = document.querySelector('.pictures');
-  let template = document.querySelector('#picture').content;
-  let newTemplate = template.querySelector('.picture');
-  let fragment = document.createDocumentFragment();
+  const spisok = document.querySelector('.pictures');
+  const template = document.querySelector('#picture').content;
+  const newTemplate = template.querySelector('.picture');
+  const fragment = document.createDocumentFragment();
 
   for (let x = 0; x < posts.length; x++) {
-    let templateClone = newTemplate.cloneNode(true);
-    let urlBlock = templateClone.querySelector('.picture__img');
+    const templateClone = newTemplate.cloneNode(true);
+    const urlBlock = templateClone.querySelector('.picture__img');
     urlBlock.src = posts[x].url;
     urlBlock.alt = posts[x].description;
-    var likesBlock = templateClone.querySelector('.picture__likes');
+    let likesBlock = templateClone.querySelector('.picture__likes');
     likesBlock.innerHTML = posts[x].likes;
-    var commentsBlock = templateClone.querySelector('.picture__comments');
+    let commentsBlock = templateClone.querySelector('.picture__comments');
     commentsBlock.innerHTML = posts[x].comments.length;
     fragment.appendChild(templateClone);
   }
@@ -20,10 +20,3 @@ function renderPhotos(posts) {
 }
 
 export { renderPhotos }
-
-
-
-
-
-
-
