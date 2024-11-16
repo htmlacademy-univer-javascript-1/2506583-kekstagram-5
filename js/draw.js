bebb05c0f6e10b52e1b1a05128e6588fb6d5a133
Данил Тотоев <totoevdanil57@gmail.com>
2024-11-16 21:44:34 +0500

    Исправляю ошибки еслинта-1

--- js/draw.js
+++ js/draw.js
@@ -1,12 +1,12 @@
 function renderPhotos(posts) {
-  var spisok = document.querySelector(".pictures");
-  var template = document.querySelector("#picture").content;
-  var newTemplate = template.querySelector(".picture");
-  var fragment = document.createDocumentFragment();
+  let spisok = document.querySelector('.pictures');
+  let template = document.querySelector('#picture').content;
+  let newTemplate = template.querySelector('.picture');
+  let fragment = document.createDocumentFragment();
 
-  for (var x = 0; x < posts.length; x++) {
-    var templateClone = newTemplate.cloneNode(true);
-    var urlBlock = templateClone.querySelector('.picture__img');
+  for (let x = 0; x < posts.length; x++) {
+    let templateClone = newTemplate.cloneNode(true);
+    let urlBlock = templateClone.querySelector('.picture__img');
     urlBlock.src = posts[x].url;
     urlBlock.alt = posts[x].description;
     var likesBlock = templateClone.querySelector('.picture__likes');
