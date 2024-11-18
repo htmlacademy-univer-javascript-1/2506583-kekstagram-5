5c1035167ebfdc08cde53c49ff32a8d1bbed406e
Данил Тотоев <totoevdanil57@gmail.com>
2024-11-18 15:24:14 +0500

    Решил без использования JSON и отрефакторил код

--- js/util.js
+++ js/util.js
@@ -2,4 +2,6 @@ function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export { getRandomInteger };
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export { getRandomInteger, isEscapeKey};
